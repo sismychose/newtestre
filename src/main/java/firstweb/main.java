a44572df093eb9316b3de1df1f3db29dfd0763ee
sismychose <sismychose@163.com>
2016-08-02 15:48:30 +0800

    index.jsp修改

--- src/main/java/firstweb/main.java
+++ src/main/java/firstweb/main.java
@@ -3,7 +3,7 @@ package firstweb;
 public class main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("jkdl");
 
 	}
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
 <h2>Hello  World !</h2>
+<h2>test</h2>
+<ruby>jclzl</ruby>
 </body>
 </html>
